845b2ffa6778de0b3505b2d800fed112d38d018d
unknown <>
2016-09-02 08:44:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/raind/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.raind.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static  final String tag="one";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
